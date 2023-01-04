a9700c79a05a021eb8fca0d839cf15bd325928c4
Rucksoly <chibuzoranthonyokenwa@gmail.com>
2023-01-04 03:27:11 +0100

    Ustuda Testing

--- ustuda_backend/settings.py
+++ ustuda_backend/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = 'ustuda_backend.wsgi.application'
 
 default_dburl = 'sqlite///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
-DATABASES = {'default':config('DATABASE_URL', default=default_dburl, cast = dburl), }
+DATABASES = {'default':default_dburl, }
 
 
 # Password validation
